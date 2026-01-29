2762115a5e00cb8f2482cd18ee5bde718e4f5667
Yaroslav <62301460+yaroslav20568@users.noreply.github.com>
2026-01-29 05:11:30 -0500

    feat: init app

---
+++ App.tsx
@@ -0,0 +1,23 @@
+import { StatusBar, StyleSheet, Text } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+
+function App() {
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <StatusBar barStyle={'dark-content'} />
+        <Text>Financial Tracker</Text>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default App;
