f6c24af8415b46a81f456dadbb52235a8a9024f2
Yaroslav <62301460+yaroslav20568@users.noreply.github.com>
2026-02-06 06:33:35 -0500

    feat: add axios interceptors

--- src/shared/api/axios.ts
+++ src/shared/api/axios.ts
@@ -1,12 +1,27 @@
 import { API_URL } from '@env';
 import axios from 'axios';
 
-const BASE_URL = API_URL;
+import { storageService } from '@/shared';
 
 export const axiosInstance = axios.create({
-  baseURL: BASE_URL,
+  baseURL: API_URL,
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json'
   }
 });
+
+axiosInstance.interceptors.request.use(
+  async config => {
+    const accessToken = await storageService.get('accessToken');
+
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
