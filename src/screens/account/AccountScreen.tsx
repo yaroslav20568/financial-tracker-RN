3a5810f84ba040aa6e71cba5551f8d8c58f58c24
Yaroslav <62301460+yaroslav20568@users.noreply.github.com>
2026-03-04 13:53:17 -0500

    refactor: delete AccountProfile

--- src/screens/account/AccountScreen.tsx
+++ src/screens/account/AccountScreen.tsx
@@ -1,16 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { HeadScreenLayout, ScreenLayout } from '@/shared';
-import { AccountProfile } from '@/widgets';
+import {
+  AccountDetails,
+  AccountHeader,
+  AccountToolbar,
+  useGetAccount
+} from '@/entities';
+import { EditAccountForm } from '@/features';
+import {
+  BorderLayout,
+  CenterLayout,
+  ErrorData,
+  HeadScreenLayout,
+  Loader,
+  ScreenLayout
+} from '@/shared';
 
 export const AccountScreen = () => {
+  const [isShowEditForm, setIsShowEditForm] = useState(false);
+  const { data, isLoading, isError, error } = useGetAccount();
+
+  const showEditForm = () => {
+    setIsShowEditForm(true);
+  };
+
+  const closeEditForm = () => {
+    setIsShowEditForm(false);
+  };
+
+  if (isLoading) {
+    return (
+      <CenterLayout>
+        <Loader />
+      </CenterLayout>
+    );
+  }
+
+  if (isError) {
+    return <ErrorData title={error.message} />;
+  }
+
   return (
     <ScreenLayout>
       <HeadScreenLayout
         title="Account Settings"
         text="Manage your account information and preferences"
       />
-      <AccountProfile />
+      <BorderLayout header={<AccountHeader id={data?.id} name={data?.name} />}>
+        <AccountToolbar
+          isShowEditForm={isShowEditForm}
+          showEditForm={showEditForm}
+        />
+        {!isShowEditForm ? (
+          <AccountDetails {...data} />
+        ) : (
+          <EditAccountForm account={data} closeEditForm={closeEditForm} />
+        )}
+      </BorderLayout>
     </ScreenLayout>
   );
 };
--- src/widgets/index.ts
+++ src/widgets/index.ts
@@ -1,3 +1,2 @@
 export * from './auth-form';
 export * from './custom-drawer';
-export * from './account-profile';
